ee8bd52b648aa458c496e92214829ab401c4632b
398552485@qq.com <398552485@qq.com>
2018-01-23 09:44:40 +0800

    完善登入页面

--- src/view/login-page.vue
+++ src/view/login-page.vue
@@ -6,20 +6,34 @@
         <img class="parting-line" src="../assets/login/parting-line.png">
       </div>
       <form class="form-horizontal" @submit.prevent="login">
-        <div class="form-group"> 登录/Login</div>
+        <div class="form-group">
+          <div class="col-md-12">登录/Login</div>
+        </div>
         <div class="form-group">
           <label for="username" class="sr-only">用户名</label>
-          <input type="text" class="form-control" id="username" name="username" v-model="username" placeholder="用户"/>
+          <div class="col-md-12">
+            <input type="text" class="form-control" id="username" name="username" v-model="username" placeholder="用户"/>
+          </div>
         </div>
         <div class="form-group">
           <label for="password" class="sr-only">密码</label>
-          <input :type="showPassword? 'text': 'password'" class="form-control" id="password" name="password" v-model="password" placeholder="密码">
+          <div class="col-md-12">
+            <input :type="showPassword? 'text': 'password'" class="form-control" id="password" name="password" v-model="password" placeholder="密码">
+          </div>
           <span @click="toggleShowPassword" :class="showPassword? 'show-password':'not-show-password'"></span>
         </div>
         <div class="form-group">
-          <button type="submit" class="btn btn-info btn-block">登录</button>
+          <label for="verifyCode" class="sr-only">验证码</label>
+          <div class="col-md-8 col-xs-8">
+            <input type="text" class="form-control verify-code" id="verifyCode" name="verifyCode" v-model="verifyCode" placeholder="验证码输入">
+          </div>
+          <div @click="getVerifyCode" class="verify-code-img pull-left"></div>
         </div>
-
+        <div class="form-group">
+          <div class="col-md-12">
+            <button type="submit" class="btn btn-info btn-block">登录</button>
+          </div>
+        </div>
       </form>
     </div>
   </div>
@@ -36,6 +50,7 @@
             return {
                 username: '',
                 password: '',
+                verifyCode: '',
                 showPassword: false
             }
         },
@@ -49,6 +64,9 @@
             },
             toggleShowPassword: function () {
               this.showPassword = !this.showPassword;
+            },
+            getVerifyCode: function () {
+
             },
             ...mapActions({
                 getUser: MutationTypes.GET_USER
@@ -106,13 +124,26 @@
         border: none;
         border-radius: 0;
         box-shadow: none;
-        border-image: linear-gradient(right,rgba(100,100,100,0), rgba(100,100,100,0.5))1 1;
+        border-image: linear-gradient(right,rgba(100,100,100,0.3), rgba(100,100,100,0.6) 50%)1 1;
         border-bottom: 1px solid #fff;
         font-size: 22px;
+        color: rgba(254,254,254, 0.8);
+        &::placeholder {
+          font-size: 22px;
+          color: rgba(106, 131, 165, 0.8);
+        }
+        &.verify-code {
+          border-image: linear-gradient(right,rgba(85,109,141,0.3), rgba(85,109,141,0.6) 80%)1 1;
+        }
+      }
+      .verify-code-img {
+        width: 100px;
+        height: 32px;
+        background-color: #3bb9dd;
       }
       .not-show-password {
         position: absolute;
-        right: 0;
+        right: 15px;
         top: 15px;
         width: 22px;
         height: 10px;
@@ -120,7 +151,7 @@
       }
       .show-password {
         position: absolute;
-        right: 0;
+        right: 15px;
         top: 15px;
         width: 22px;
         height: 15px;
