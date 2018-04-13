65937ac9422f76cc82604867e5d8d50f002c8ade
liliangfeng <398552485@qq.com>
2018-04-13 19:38:07 +0800

    图片

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
  * Created by spring on 2018/1/17.
  */
 let config = {
-    URL_API: 'http://192.168.0.112:20001/',
+    URL_API: 'http://192.168.0.12:20001/',
     // URL_API: 'http://118.178.95.186:20001',
-    LAMP_URL_API: 'http://192.168.0.112:20002/',
+    LAMP_URL_API: 'http://192.168.0.12:20002/',
     // LAMP_URL_API: 'http://118.178.95.186:20002',
     // LAMP_URL_API: 'http://192.168.0.134:20002/',
     // URL_API: 'http://www.zeta-alliance.com:20001',
